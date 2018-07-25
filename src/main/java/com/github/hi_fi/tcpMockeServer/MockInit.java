ab538164666e2dcc19eae39d41db995de21d5fba
Juho Saarinen <juho.saarinen@gmail.com>
2018-07-25 12:50:14 +0300

    Starting bits to parser

--- src/main/java/com/github/hi_fi/tcpMockeServer/MockInit.java
+++ src/main/java/com/github/hi_fi/tcpMockeServer/MockInit.java
@@ -1,37 +1,27 @@
 package com.github.hi_fi.tcpMockeServer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import javax.annotation.PostConstruct;
-
+import com.github.hi_fi.tcpMockeServer.data.FieldsToClear;
+import com.github.hi_fi.tcpMockeServer.data.RegexpFilters;
+import com.github.hi_fi.tcpMockeServer.model.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.handler.ServiceActivatingHandler;
 import org.springframework.integration.ip.tcp.TcpInboundGateway;
 import org.springframework.integration.ip.tcp.TcpOutboundGateway;
 import org.springframework.integration.ip.tcp.connection.TcpNetClientConnectionFactory;
-import org.springframework.integration.ip.tcp.connection.TcpNetServerConnectionFactory;
-import org.springframework.integration.ip.tcp.serializer.ByteArrayRawSerializer;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import org.springframework.integration.ip.tcp.connection.TcpNioServerConnectionFactory;
 
-import com.github.hi_fi.tcpMockeServer.data.FieldsToClear;
-import com.github.hi_fi.tcpMockeServer.model.Mock;
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @ConfigurationProperties(prefix="mock")
@@ -62,6 +52,9 @@ public class MockInit {
 	@Autowired
 	private FieldsToClear ftc;
 
+	@Autowired
+	private RegexpFilters rf;
+
 	@PostConstruct
 	public void init() {
 		AutowireCapableBeanFactory bf = this.applicationContext.getAutowireCapableBeanFactory();
@@ -75,18 +68,19 @@ public class MockInit {
 	
 	public void startMock(Mock mock) {
 		if(!mockServices.containsKey(mock.getName())){
-			//ConnectionFactory for mock's incoming port
-			BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(TcpNetServerConnectionFactory.class);
-			builder.addConstructorArgValue(mock.getMockPort());
-			builder.addPropertyValue("serializer", new TcpMockServerSerializer(10000));
-			builder.addPropertyValue("deserializer", new TcpMockServerSerializer(10000));
-			beanFactory.registerBeanDefinition(mock.getName() + "MockConnFactory", builder.getBeanDefinition());
-			//Gateway for mock's incoming requests
-			builder = BeanDefinitionBuilder.rootBeanDefinition(TcpInboundGateway.class);
-			builder.addPropertyReference("connectionFactory", mock.getName() + "MockConnFactory");
-			builder.addPropertyReference("requestChannel", mock.getName() + "MockIncomingChannel");
-			beanFactory.registerBeanDefinition(mock.getName() + "IncomingGateway", builder.getBeanDefinition());
-			mockServices.put(mock.getName(), mock.getName() + "IncomingGateway");
+            BeanDefinitionBuilder builder;
+                //ConnectionFactory for mock's incoming port
+                builder = BeanDefinitionBuilder.rootBeanDefinition(TcpNioServerConnectionFactory.class);
+                builder.addConstructorArgValue(mock.getMockPort());
+                builder.addPropertyValue("serializer", new TcpMockServerSerializer(100000, mock.getMessageStarter()));
+                builder.addPropertyValue("deserializer", new TcpMockServerSerializer(100000, mock.getMessageStarter()));
+                beanFactory.registerBeanDefinition(mock.getName() + "MockConnFactory", builder.getBeanDefinition());
+                //Gateway for mock's incoming requests
+                builder = BeanDefinitionBuilder.rootBeanDefinition(TcpInboundGateway.class);
+                builder.addPropertyReference("connectionFactory", mock.getName() + "MockConnFactory");
+                builder.addPropertyReference("requestChannel", mock.getName() + "MockIncomingChannel");
+                beanFactory.registerBeanDefinition(mock.getName() + "IncomingGateway", builder.getBeanDefinition());
+                mockServices.put(mock.getName(), mock.getName() + "IncomingGateway");
 			//Enricher that adds mocks information to message headers. This created from channel automatically.
 			IntegrationFlow flow = IntegrationFlows.from(mock.getName() + "MockIncomingChannel")
 							.enrichHeaders(h -> h.header("mockName", mock.getName()))
@@ -95,6 +89,7 @@ public class MockInit {
 			beanFactory.registerSingleton(mock.getName() + "IncomingFlow", flow);
             beanFactory.initializeBean(flow, mock.getName() + "IncomingFlow");
             ftc.setFields(mock.getName(), mock.getFieldsToClear());
+			rf.setRegexpFilters(mock.getName(), mock.getRegexpFilters());
 		}
 		((TcpInboundGateway) beanFactory.getBean(mockServices.get(mock.getName()))).start();
 	}
@@ -105,8 +100,8 @@ public class MockInit {
 			BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(TcpNetClientConnectionFactory.class);
 			builder.addConstructorArgValue(mock.getTargetHost());
 			builder.addConstructorArgValue(mock.getTargetPort());
-			builder.addPropertyValue("serializer", new TcpMockServerSerializer(10000));
-			builder.addPropertyValue("deserializer", new TcpMockServerSerializer(10000));
+			builder.addPropertyValue("serializer", new TcpMockServerSerializer(100000, mock.getMessageStarter()));
+			builder.addPropertyValue("deserializer", new TcpMockServerSerializer(100000, mock.getMessageStarter()));
 			beanFactory.registerBeanDefinition(mock.getName() + "TargetConnFactory", builder.getBeanDefinition());
 			
 			//Gateway for mock's connection to real endpoint
